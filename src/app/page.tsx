29095253db0c384584ed5ad0593b3fab07fa58b0
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-11-22 21:51:40 +0100

    Add whole app header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Suspense } from "react";
 import MainPageContent from "./main-page-content";
-import MainPageHeader from "./main-page-header";
 
 // export const dynamic = "force-dynamic";
 
-export default async function Home() {
+export default function Home() {
     return (
         <main className="h-fit w-full bg-slate-800 text-white">
-            <MainPageHeader />
             <Suspense fallback={<div>Loading...</div>}>
                 <MainPageContent />
             </Suspense>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,18 +2,19 @@
 
 import { unstable_cacheLife } from "next/cache";
 
-export default async function MainPageHeader() {
+export default async function Header() {
     unstable_cacheLife({
         stale: 5,
         revalidate: 5,
+        // expire: 10, // this throws
     });
 
     const date = new Date();
     const lastRenderedAt: string = date.toISOString().slice(11, 23);
 
     return (
-        <div className="">
-            Main page header last rendered at: {lastRenderedAt}
-        </div>
+        <header className="w-full bg-slate-700 p-5 text-center font-bold text-white">
+            Header of all pages. Last rendered at: {lastRenderedAt}
+        </header>
     );
 }
