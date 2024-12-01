2b3496fade37d25cfbe2193257ea2c4aa819aa63
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-12-01 21:24:18 +0100

    Add template single starter

---
+++ src/app/(commonHeader)/templates/[templateId]/_components/var-decider.tsx
@@ -0,0 +1,3 @@
+export default function VarDecider() {
+    return <div>var-decider</div>;
+}
--- src/app/(commonHeader)/templates/[templateId]/page.tsx
+++ src/app/(commonHeader)/templates/[templateId]/page.tsx
@@ -1,4 +1,6 @@
-import TemplateContent from "./template-content";
+import { getTemplate } from "@/data-access/templates";
+import { redirect } from "next/navigation";
+// import { getTemplates } from "../templates-content";
 
 type Props = {
     params: Promise<{
@@ -6,7 +8,28 @@ type Props = {
     }>;
 };
 
-// always dynamic bc of the params
-export default function Templates({ params }: Props) {
-    return <TemplateContent params={params} />;
+// export const dynamic = "force-dynamic";
+
+// export const generateStaticParams = async (): Promise<
+//     { templateId: string }[]
+// > => {
+//     const templates = await getTemplates();
+//     return templates.map((template) => ({
+//         templateId: template.id,
+//     }));
+// };
+
+export default async function Templates({ params }: Props) {
+    const { templateId } = await params;
+    const template = await getTemplate(templateId);
+
+    if (!template) redirect("/templates");
+
+    return (
+        <main className="grid h-full w-full place-items-center">
+            <div className="flex flex-col items-center gap-8">
+                <h1 className="text-xl font-bold">Template: {template.name}</h1>
+            </div>
+        </main>
+    );
 }
