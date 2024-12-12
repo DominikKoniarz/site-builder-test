b3c8f0146346d744dcfafeb3b0fca179e7a4fbb6
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-12-12 20:22:10 +0100

    Use prisma enum

--- src/components/templates/variables/common-inputs.tsx
+++ src/components/templates/variables/common-inputs.tsx
@@ -14,6 +14,7 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 import useTemplateForm from "@/hooks/useTemplateForm";
+import { VariableType } from "@prisma/client";
 
 export default function CommonInputs({ index }: { index: number }) {
     const { control } = useTemplateForm();
@@ -62,10 +63,12 @@ export default function CommonInputs({ index }: { index: number }) {
                                 </SelectTrigger>
                             </FormControl>
                             <SelectContent>
-                                <SelectItem value="TEXT">
+                                <SelectItem value={VariableType.TEXT}>
                                     Zmienna tekstowa
                                 </SelectItem>
-                                <SelectItem value="BANNER">Banner</SelectItem>
+                                <SelectItem value={VariableType.BANNER}>
+                                    Banner
+                                </SelectItem>
                             </SelectContent>
                         </Select>
                         <FormMessage />
