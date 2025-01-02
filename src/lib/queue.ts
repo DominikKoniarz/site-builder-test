bf5e86badb7e302d602cf810dbb505bbae4da464
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-01-02 21:03:58 +0100

    Add queue

---
+++ src/lib/queue.ts
@@ -0,0 +1,53 @@
+type Task = () => Promise<void> | void;
+
+export class Queue {
+    private tasks: Task[] = [];
+    private isProcessing: boolean = false;
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    addTask(task: Task) {
+        this.tasks.push(task);
+    }
+
+    addTasks(tasks: Task[]) {
+        this.tasks.push(...tasks);
+    }
+
+    start() {
+        this.processTasks();
+    }
+
+    getQueueLength() {
+        return this.tasks.length;
+    }
+
+    private async processTasks() {
+        if (this.isProcessing || this.tasks.length === 0) return;
+
+        this.isProcessing = true;
+
+        while (this.tasks.length > 0) {
+            const task = this.tasks.shift();
+            if (!task) break;
+
+            try {
+                const result = task();
+
+                if (result instanceof Promise) {
+                    await result;
+                }
+            } catch (err) {
+                console.error(
+                    `Error processing task in queue ${this.name}:`,
+                    err,
+                );
+            }
+        }
+
+        this.isProcessing = false;
+    }
+}
