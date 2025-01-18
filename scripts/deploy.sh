7b471abefd27290c2ad5367baa7abd30ec947ba0
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-01-18 12:47:12 +0100

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,11 @@ git stash
 git pull
 git stash drop
 export PATH=/root/.local/share/pnpm:$PATH
-# export PATH=/root/.local/share/pnpm/pnpm:$PATH
+export PATH=/root/.local/share/pnpm/pnpm:$PATH
 pnpm -v
 pnpm install
 pnpm prisma generate
 pnpm run build
 # pm2 start ecosystem.config.js
-# pm2 start npm --name "nextjs-app" -- start
-pnpm start
+pm2 start npm --name "nextjs-app" -- start
+# pnpm start
