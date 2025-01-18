25ed0aa9f4cb0fc46ba3a521ade6dca0ab43b93a
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-01-18 21:46:55 +0100

    Maybe fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,16 @@ module.exports = {
     apps: [
         {
             name: "nextjs-app",
-            script: "'pnpm start'",
+            script: "./node_modules/next/dist/bin/next start",
+            //script: "'pnpm start'",
             // args: "start",
             exec_mode: "cluster",
             instances: 2,
+            increment_var: "PORT",
+            env: {
+                PORT: 3000,
+                NODE_ENV: "production",
+            },
         },
     ],
 };
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,4 @@ git stash drop
 pnpm install
 pnpm prisma generate
 pnpm run build
-# pnpm run pm2 delete nextjs-app
 pm2 start ecosystem.config.js
