c458392302406297741aea3bbfdeca4f656a5923
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-01-19 23:00:43 +0100

    Fix condition

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,7 +3,9 @@ import { tmpImagesCron } from "./lib/images/cron";
 
 export function register() {
     console.log(env.INSTANCE_ID, process.env.INSTANCE_ID);
-    if (!env.INSTANCE_ID || env.INSTANCE_ID === "1") tmpImagesCron.start();
+    // Only start in dev or prod, but first instance
+    if (env.INSTANCE_ID === undefined || env.INSTANCE_ID === "0")
+        tmpImagesCron.start();
 
     console.log(
         "Tmp images cron job started. Next invocation:",
