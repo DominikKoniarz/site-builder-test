3d1074e790e94486177691a369c3847fb6f8007f
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-01-19 23:03:11 +0100

    Remove comment
    Clean up

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,14 +1,14 @@
-import { env } from "./env";
 import { tmpImagesCron } from "./lib/images/cron";
+import { env } from "./env";
 
 export function register() {
-    console.log(env.INSTANCE_ID, process.env.INSTANCE_ID);
     // Only start in dev or prod, but first instance
-    if (env.INSTANCE_ID === undefined || env.INSTANCE_ID === "0")
+    if (env.INSTANCE_ID === undefined || env.INSTANCE_ID === "0") {
         tmpImagesCron.start();
 
-    console.log(
-        "Tmp images cron job started. Next invocation:",
-        tmpImagesCron.nextDate().toISO(),
-    );
+        console.log(
+            "Tmp images clean up cron job started. Next invocation:",
+            tmpImagesCron.nextDate().toISO(),
+        );
+    }
 }
