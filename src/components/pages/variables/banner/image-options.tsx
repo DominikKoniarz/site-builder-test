5dd5dfc9f25777b82c2b486fb08682e98b3ce204
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-02-18 21:53:46 +0100

    Show image options on mobile device

--- src/components/pages/variables/banner/image-options.tsx
+++ src/components/pages/variables/banner/image-options.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function ImageOptions({ imageIndex, src }: Props) {
     return (
         <div className="group absolute left-1/2 top-1/2 grid h-full w-full -translate-x-1/2 -translate-y-1/2 place-items-center bg-transparent">
-            <div className="flex flex-row gap-2.5 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+            <div className="flex flex-row gap-2.5 opacity-100 transition-opacity duration-200 sm:opacity-0 sm:group-hover:opacity-100">
                 <CropImageDialog src={src} imageIndex={imageIndex} />
                 <DeleteImageDialog imageIndex={imageIndex} />
             </div>
